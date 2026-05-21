842c9e4f0e66fddd5b4bac16c080196109c223ad
TrueWinter <nicholis@truewinter.dev>
2026-05-21 22:52:07 +0200

    Added build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -r dist 2&>/dev/null
+
+COMMIT=$(git rev-parse --short HEAD)
+UNRESOLVED=$(git status -s)
+if [ "$UNRESOLVED" != "" ]; then
+    COMMIT="${COMMIT}-indev"
+fi
+
+build() {
+  DIST_NAME="dist/dns-proxy-${GOOS}-${GOARCH}"
+  if [ "$GOOS" == "windows" ]; then
+    DIST_NAME=$DIST_NAME.exe
+  fi
+  go build -ldflags "-X main.Version=${COMMIT}" -o $DIST_NAME .
+}
+
+GOOS=windows GOARCH=amd64 build
+GOOS=linux GOARCH=amd64 build
